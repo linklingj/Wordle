8d13a95d11a355909ad6724cea31f490cd737651
unknown <jaehyunc413@gmail.com>
2022-05-28 23:45:33 +0900

    get api

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    getNewWord();
     createSquares();
-    const answer = "crane";
+
+    let answer;
     let finished = false;
     let guessedWordCount = 0;
     const guessedWords = [[]];
     const keys = document.querySelectorAll('.keyboard-row button');
+
+    function getNewWord() {
+        fetch(
+            `https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=5&lettersMax=5`,
+            {
+                method: "GET",
+                headers: {
+                    'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com',
+                    'X-RapidAPI-Key': '4c4ec7c47amsh928a185b096cbeep108e6cjsnbeb3259acee8'
+                },
+            }
+        )
+        .then((response) => {
+            return response.json();
+        })
+        .then((res) => {
+            answer = res.word;
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+    }
 
     for (let i = 0; i < keys.length; i++) {
         keys[i].onclick = ({target}) => {
@@ -18,6 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 return;
             }
             if (key === "del") {
+                handleDelete();
                 return;
             }
             updateGuessedWords(key);
@@ -31,36 +55,46 @@ document.addEventListener("DOMContentLoaded", () => {
             return;
         }
         const currentWord = currentWordArr.join("");
-        const wordExist = true;
-        if (wordExist === false) {
+        fetch(`https://wordsapiv1.p.rapidapi.com/words/${currentWord}`, {
+            method: "GET",
+            headers: {
+                "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+                'X-RapidAPI-Key': '4c4ec7c47amsh928a185b096cbeep108e6cjsnbeb3259acee8',
+            },
+        })
+        .then((res) => {
+            if (!res.ok) {
+                throw Error();
+            }
+
+            guessedWordCount++;
+            if (currentWord === answer) {
+                window.alert("Correct");
+                finished = true;
+            }
+            else {
+                if (guessedWordCount === 6) {
+                    window.alert(`Sorry, you have no more guesses! The word is ${answer}.`);
+                    finished = true;
+                } else {
+                    guessedWords.push([]);
+                }
+            }
+    
+            currentWordArr.forEach((letter,index) => {
+                setTimeout(() => {
+                    const tileColor = getTileColor(letter,index);
+                    const letterId = (guessedWordCount-1) * 5 + 1 + index;
+                    const letterEl = document.getElementById(letterId);
+                    letterEl.classList.add(tileColor);
+                    letterEl.classList.add("animate__flipInX");
+                }, 200 * index);
+            });
+        })
+        .catch(() => {
             window.alert("Not in word list");
             return;
-        }
-
-        guessedWordCount++;
-        if (currentWord === answer) {
-            window.alert("Correct");
-            finished = true;
-        }
-        else {
-            if (guessedWordCount === 6) {
-                window.alert('Sorry, you have no more guesses! The word is ${answer}.');
-                finished = true;
-            } else {
-                guessedWords.push([]);
-            }
-        }
-
-        currentWordArr.forEach((letter,index) => {
-            setTimeout(() => {
-                const tileColor = getTileColor(letter,index);
-                const letterId = (guessedWordCount-1) * 5 + 1 + index;
-                const letterEl = document.getElementById(letterId);
-                letterEl.innerText = letter;
-                letterEl.classList.add(tileColor);
-                letterEl.classList.add("animate__flipInX");
-            }, 200 * index);
-        });
+        })
     }
 
     function getTileColor(letter,index) {
@@ -85,15 +119,27 @@ document.addEventListener("DOMContentLoaded", () => {
         if (currentWordArr && currentWordArr.length < 5) {
             currentWordArr.push(letter);
         }
+        const letterId = guessedWordCount * 5 + currentWordArr.length;
+        const letterEl = document.getElementById(letterId);
+        letterEl.innerText = letter;
     }
 
-    
+    function handleDelete() {
+        const currentWordArr = getCurrentWordArr();
+        if (currentWordArr.length >= 1) {
+            const letterId = guessedWordCount * 5 + currentWordArr.length;
+            const letterEl = document.getElementById(letterId);
+            letterEl.innerText = null;
+            currentWordArr.pop();
+        }
+    }
 
     function createSquares() {
         const gameBoard = document.getElementById("board");
         for (let index = 0; index < 30; index++) {
             let square = document.createElement("div");
             square.classList.add("square");
+            square.classList.add("animate__animated");
             square.setAttribute("id", index+1);
             gameBoard.appendChild(square);
         }
